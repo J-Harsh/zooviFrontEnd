4e53f471129fd767444b474a18fc2bfa37340454
harsh <harshjain172001@gmail.com>
2022-11-09 02:17:16 +0530

    empty commit

--- src/pages/Shopping.js
+++ src/pages/Shopping.js
@@ -13,7 +13,7 @@ const Shopping = () => {
         <div className='content'>
           <h1>Search Results</h1>
           <div className='filterContainer'>
-
+            
           </div>
           <div className='productContainer'>
 
